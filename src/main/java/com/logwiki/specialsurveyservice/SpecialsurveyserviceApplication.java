a6d46a0ec51e267d2e1bc91826243f6bb9b73658
slbin <smpts00@naver.com>
2023-07-18 17:02:21 +0900

    init

---
+++ src/main/java/com/logwiki/specialsurveyservice/SpecialsurveyserviceApplication.java
@@ -0,0 +1,13 @@
+package com.logwiki.specialsurveyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialsurveyserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialsurveyserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logwiki/specialsurveyservice/SpecialsurveyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logwiki.specialsurveyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecialsurveyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
