31108d880fcf75f000c12e4b9e8ff0c2f77c16e1
yeonjae <duswo0624@naver.com>
2023-07-19 08:41:13 +0900

    :sparkles: 스프링 시큐리티 임시적으로 비활성화

--- src/main/java/com/logwiki/specialsurveyservice/SpecialsurveyserviceApplication.java
+++ src/main/java/com/logwiki/specialsurveyservice/SpecialsurveyserviceApplication.java
@@ -2,8 +2,9 @@ package com.logwiki.specialsurveyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpecialsurveyserviceApplication {
 
 	public static void main(String[] args) {
