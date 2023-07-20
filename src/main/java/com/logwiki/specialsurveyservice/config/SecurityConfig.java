c12cf106aad8704d80141a26be17dd51bd349c5d
amenable <duswo0624@naver.com>
2023-07-20 15:34:40 +0900

    :sparkles: 요청에 대한 설정 추가
    - CSRF 비활성화
    - 회원가입 할 때 권한 확인 안하기
    - H2 DB 접근할 때 권한 확인 안하기

--- src/main/java/com/logwiki/specialsurveyservice/config/SecurityConfig.java
+++ src/main/java/com/logwiki/specialsurveyservice/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.logwiki.specialsurveyservice.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -14,8 +20,35 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+  private final CorsFilter corsFilter;
+
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.disable())
+
+        .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+
+        .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+            .requestMatchers("/api/signup").permitAll()
+            .requestMatchers(PathRequest.toH2Console()).permitAll()
+            .anyRequest().authenticated())
+
+        .sessionManagement(sessionManagement ->
+            sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        )
+
+        .headers(headers ->
+            headers.frameOptions(options ->
+                options.sameOrigin()
+            )
+        );
+
+    return http.build();
   }
 }
