c9efaa3dad9148a29f4e885ca382b27c5ab37180
Amenable-C <duswo0624@naver.com>
2023-07-22 22:40:22 +0900

    :sparkles: 회원 조회 기능 추가

--- src/main/java/com/logwiki/specialsurveyservice/api/controller/account/AccountController.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/controller/account/AccountController.java
@@ -7,6 +7,9 @@ import com.logwiki.specialsurveyservice.api.utils.ApiResponse;
 import com.logwiki.specialsurveyservice.api.utils.ApiUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +28,17 @@ public class AccountController {
     ) {
         return ApiUtils.success(
                 accountService.signup(accountCreateRequest.toServiceRequest()));
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ApiResponse<AccountResponse> getMyUserInfo() {
+        return ApiUtils.success(accountService.getMyUserWithAuthorities());
+    }
+
+    @GetMapping("/user/{email}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ApiResponse<AccountResponse> getUserInfo(@PathVariable String email) {
+        return ApiUtils.success(accountService.getUserWithAuthorities(email));
     }
 }
---
+++ src/main/java/com/logwiki/specialsurveyservice/api/utils/SecurityUtil.java
@@ -0,0 +1,32 @@
+package com.logwiki.specialsurveyservice.api.utils;
+
+import com.logwiki.specialsurveyservice.exception.NotFoundAuthenticationException;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Slf4j
+public class SecurityUtil {
+
+    private SecurityUtil() {}
+
+    public static Optional<String> getCurrentUsername() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null) {
+            throw new NotFoundAuthenticationException("인증 정보가 등록되어 있지 않습니다.");
+        }
+
+        String username = null;
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+            username = springSecurityUser.getUsername();
+        } else if (authentication.getPrincipal() instanceof String) {
+            username = (String) authentication.getPrincipal();
+        }
+
+        return Optional.ofNullable(username);
+    }
+}
