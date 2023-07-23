c45c6d4d4c667185852496c016c8f14eae5090ee
slbin-park <smpts00@naver.com>
2023-07-23 21:55:05 +0900

    :sparkles: 카테고리 타입 enum 상수로 관리하도록 변경

--- src/main/java/com/logwiki/specialsurveyservice/domain/questioncategory/QuestionCategory.java
+++ src/main/java/com/logwiki/specialsurveyservice/domain/questioncategory/QuestionCategory.java
@@ -1,6 +1,11 @@
 package com.logwiki.specialsurveyservice.domain.questioncategory;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,11 +21,11 @@ public class QuestionCategory {
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    private QuestionCategoryType questionCategoryType;
+    private QuestionCategoryType type;
 
     @Builder
-    public QuestionCategory(QuestionCategoryType questionCategoryType) {
-        this.questionCategoryType = questionCategoryType;
+    public QuestionCategory(QuestionCategoryType type) {
+        this.type = type;
     }
 
 }
