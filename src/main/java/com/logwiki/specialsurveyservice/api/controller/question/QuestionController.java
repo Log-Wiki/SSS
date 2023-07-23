5f5fc7e98c53813466d9b03b2473b63d83e46455
slbin-park <smpts00@naver.com>
2023-07-24 01:37:00 +0900

    :fire: 테스트 api 삭제

--- src/main/java/com/logwiki/specialsurveyservice/api/controller/question/QuestionController.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/controller/question/QuestionController.java
@@ -1,6 +1,5 @@
 package com.logwiki.specialsurveyservice.api.controller.question;
 
-import com.logwiki.specialsurveyservice.api.controller.question.request.MultipleChoiceCreateRequest;
 import com.logwiki.specialsurveyservice.api.controller.question.request.QuestionModifyRequest;
 import com.logwiki.specialsurveyservice.api.service.question.QuestionService;
 import com.logwiki.specialsurveyservice.api.utils.ApiResponse;
@@ -8,7 +7,6 @@ import com.logwiki.specialsurveyservice.api.utils.ApiUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuestionController {
 
     private final QuestionService questionService;
-
-    @PostMapping("/question")
-    public ApiResponse<?> question(
-            @Valid @RequestBody MultipleChoiceCreateRequest dto) {
-        return ApiUtils.success(dto);
-    }
 
     @PutMapping("/question/{id}")
     public ApiResponse<?> questionModify(@PathVariable Long id,
