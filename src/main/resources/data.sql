70581f94681f4d3d8b27049ed993b1699b708f71
slbin-park <smpts00@naver.com>
2023-07-25 00:41:27 +0900

    :sparkles: account_code 기본값 추가 , 초기 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,36 @@
-insert into authority (type) values ('ROLE_USER');
-insert into authority (type) values ('ROLE_ADMIN');
+insert into authority (type)
+values ('ROLE_USER');
+insert into authority (type)
+values ('ROLE_ADMIN');
 
 insert into account (email, password, gender, name, phone_number, response_survey_count, create_survey_count, winning_giveaway_count, point, birthday, create_at, modified_at)
 values ('admin@naver.com', '$2a$10$SuhlmaC1Rfyn4F7tc0x8duB9YkDjU1PXY2BXI7xtc08hjsI/RmzF.', 'MALE', '최주인', '010-1111-2222', 0, 0, 0, 0, now(), now(), now());
 insert into account (email, password, gender, name, phone_number, response_survey_count, create_survey_count, winning_giveaway_count, point, birthday, create_at, modified_at)
 values ('user@naver.com', '$2a$10$s493xSE6O4c6kS.TStPjIuiuF0nHXjhemDH3YaSzzIBdFb54EuZNS', 'FEMALE', '김유저', '010-3333-4444', 0, 0, 0, 0, now(), now(), now());
 
-insert into account_authority (account_id, authority_id, create_at, modified_at) values (1, 1, now(), now());
-insert into account_authority (account_id, authority_id, create_at, modified_at) values (1, 2, now(), now());
-insert into account_authority (account_id, authority_id, create_at, modified_at) values (2, 1, now(), now());
+insert into account_authority (account_id, authority_id, create_at, modified_at)
+values (1, 1, now(), now());
+insert into account_authority (account_id, authority_id, create_at, modified_at)
+values (1, 2, now(), now());
+insert into account_authority (account_id, authority_id, create_at, modified_at)
+values (2, 1, now(), now());
+
+insert into account_code (type)
+values ('MAN');
+insert into account_code (type)
+values ('WOMAN');
+insert into account_code (type)
+values ('UNDER_TEENS');
+insert into account_code (type)
+values ('TEENS');
+insert into account_code (type)
+values ('TWENTIES');
+insert into account_code (type)
+values ('THIRTIES');
+insert into account_code (type)
+values ('FORTIES');
+insert into account_code (type)
+values ('FIFTIES');
+insert into account_code (type)
+values ('SIXTIES');
+
