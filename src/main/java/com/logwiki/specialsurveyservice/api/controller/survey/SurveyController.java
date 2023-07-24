4fc45f275753306b215a05f7cff26caa1cac2327
slbin-park <smpts00@naver.com>
2023-07-24 23:15:59 +0900

    :sparkles: 설문조사 저장 기능 구현

--- src/main/java/com/logwiki/specialsurveyservice/api/controller/survey/SurveyController.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/controller/survey/SurveyController.java
@@ -1,7 +1,14 @@
 package com.logwiki.specialsurveyservice.api.controller.survey;
 
+import com.logwiki.specialsurveyservice.api.controller.survey.request.SurveyCreateRequest;
+import com.logwiki.specialsurveyservice.api.service.survey.SurveyService;
+import com.logwiki.specialsurveyservice.api.utils.ApiResponse;
+import com.logwiki.specialsurveyservice.api.utils.ApiUtils;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class SurveyController {
 
+    private final SurveyService surveyService;
+
     @PostMapping("/survey")
-    public String surveyAdd() {
-        return "";
+    public ApiResponse<?> surveyAdd(@Valid @RequestBody SurveyCreateRequest dto, Authentication authentication) {
+        String userId = authentication.getName();
+        surveyService.addSurvey(userId, dto.from());
+        return ApiUtils.success(dto.from());
     }
 }
