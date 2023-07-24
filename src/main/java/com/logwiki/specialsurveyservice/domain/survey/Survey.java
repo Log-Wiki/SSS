a122a43fdb502e2c90bcb43ba661126c22f001ef
slbin <smpts00@naver.com>
2023-07-24 16:41:44 +0900

    :sparkles: Survey BaseEntity 상속 추가

--- src/main/java/com/logwiki/specialsurveyservice/domain/survey/Survey.java
+++ src/main/java/com/logwiki/specialsurveyservice/domain/survey/Survey.java
@@ -1,17 +1,22 @@
 package com.logwiki.specialsurveyservice.domain.survey;
 
+import com.logwiki.specialsurveyservice.domain.BaseEntity;
 import com.logwiki.specialsurveyservice.domain.surveycategory.SurveyCategory;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Survey {
+public class Survey extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,5 +36,5 @@ public class Survey {
 
     @ManyToOne(fetch = FetchType.LAZY)
     private SurveyCategory surveyCategory;
-    
+
 }
