71c98362f54b224873121533b44864982d79e266
kss4037 <49977437+kss4037@users.noreply.github.com>
2023-07-25 09:07:54 +0900

    Revert "Feature/주문결제예외처리"

--- src/main/java/com/logwiki/specialsurveyservice/api/service/payment/AuthenticationPaymentService.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/service/payment/AuthenticationPaymentService.java
@@ -4,7 +4,9 @@ import com.logwiki.specialsurveyservice.api.service.payment.request.PaymentAuthe
 import com.logwiki.specialsurveyservice.api.service.payment.response.PaymentResponse;
 import com.logwiki.specialsurveyservice.domain.orders.Orders;
 import com.logwiki.specialsurveyservice.domain.orders.OrdersRepository;
-import com.logwiki.specialsurveyservice.exception.BaseException;
+import com.logwiki.specialsurveyservice.exception.DuplicatedAccountException;
+import com.logwiki.specialsurveyservice.exception.IamportApiException;
+import com.logwiki.specialsurveyservice.exception.NotVaildAmountException;
 import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.exception.IamportResponseException;
 import com.siot.IamportRestClient.response.IamportResponse;
@@ -31,21 +33,21 @@ public class AuthenticationPaymentService {
         Orders order = orderRepository.findOneByOrderId(request.getOrderId()).orElse(null);
 
         if (order == null) {
-            throw new BaseException("주문 정보가 없는 결제인증 요청입니다." , 2000);
+            throw new DuplicatedAccountException("주문 정보가 없는 결제인증 요청입니다.");
         }
 
-        IamportResponse<Payment> iamportResponse;
+        IamportResponse<Payment> iamportResponse = null;
         try {
             iamportResponse = iamportClientApi.paymentByImpUid(request.getImp_uid());
         } catch (IamportResponseException e) {
-            throw new BaseException("iamport 응답 예외입니다." , 2001);
+            throw new IamportApiException("iamport 응답 예외입니다.");
         } catch (IOException e) {
-            throw new BaseException("iamport IO 예외입니다." , 2002);
+            throw new IamportApiException("iamport IO 예외입니다.");
         }
 
         try {
             if (iamportResponse.getResponse().getAmount().intValue() != order.getOrderAmount()) {
-                throw new BaseException("주문 금액과 결제금액이 다릅니다.", 2003);
+                throw new NotVaildAmountException("주문 금액과 결제금액이 다릅니다.");
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
