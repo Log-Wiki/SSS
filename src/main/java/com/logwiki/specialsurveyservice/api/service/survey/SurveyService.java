238d1a2fba7c0af1a4a42c231f86af44fc67a48e
amenable <duswo0624@naver.com>
2023-07-25 17:43:49 +0900

    :poop: 설문 생성 시 당첨 상품 등록 서비스 로직 추가

--- src/main/java/com/logwiki/specialsurveyservice/api/service/survey/SurveyService.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/service/survey/SurveyService.java
@@ -4,12 +4,16 @@ package com.logwiki.specialsurveyservice.api.service.survey;
 import com.logwiki.specialsurveyservice.api.service.survey.request.SurveyCreateServiceRequest;
 import com.logwiki.specialsurveyservice.domain.account.Account;
 import com.logwiki.specialsurveyservice.domain.account.AccountRepository;
+import com.logwiki.specialsurveyservice.domain.giveaway.GiveawayRepository;
 import com.logwiki.specialsurveyservice.domain.survey.Survey;
 import com.logwiki.specialsurveyservice.domain.survey.SurveyRepository;
 import com.logwiki.specialsurveyservice.domain.surveycategory.SurveyCategory;
 import com.logwiki.specialsurveyservice.domain.surveycategory.SurveyCategoryRepository;
+import com.logwiki.specialsurveyservice.domain.surveygiveaway.SurveyGiveaway;
 import com.logwiki.specialsurveyservice.exception.BaseException;
 import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +28,8 @@ public class SurveyService {
 
     private final SurveyCategoryRepository surveyCategoryRepository;
 
+    private final GiveawayRepository giveawayRepository;
+
     public void addSurvey(String userEmail, SurveyCreateServiceRequest dto) {
         Account account = accountRepository.findOneWithAuthoritiesByEmail(userEmail)
                 .orElseThrow(() -> new BaseException("유저를 찾지못했습니다.", 1000));
@@ -32,5 +38,16 @@ public class SurveyService {
                 .build();
         Survey survey = surveyRepository.save(dto.toEntity(account.getId()));
         survey.addCategory(surveyCategory);
+
+        // 설문에 당첨 상품 등록
+        List<SurveyGiveaway> surveyGiveaways = dto.getGiveaways().stream()
+                .map(giveaway -> SurveyGiveaway.create(giveaway.getCount(), survey,
+                        giveawayRepository.findById(giveaway.getId())
+                                .orElseThrow(
+                                        () -> new BaseException("당첨 상품의 PK 값이 올바르지 않습니다.", 1000))))
+                .collect(Collectors.toList());
+
+
+        survey.addSurveyGiveaways(surveyGiveaways);
     }
 }
