d48968bfbc836d2b3d086a2352fddf6d02b14dad
amenable <duswo0624@naver.com>
2023-07-26 12:02:11 +0900

    :white_check_mark: 설문 제작 시 당첨 번호 저장 테스트 작성

--- src/test/java/com/logwiki/specialsurveyservice/api/service/targetnumber/TargetNumberServiceTest.java
+++ src/test/java/com/logwiki/specialsurveyservice/api/service/targetnumber/TargetNumberServiceTest.java
@@ -1,10 +1,13 @@
 package com.logwiki.specialsurveyservice.api.service.targetnumber;
 
+import static org.assertj.core.api.Assertions.*;
+
 import com.logwiki.specialsurveyservice.IntegrationTestSupport;
+import com.logwiki.specialsurveyservice.api.service.survey.request.GiveawayAssignServiceRequest;
 import com.logwiki.specialsurveyservice.api.service.targetnumber.request.TargetNumberCreateServiceRequest;
-import java.util.HashMap;
-import java.util.Map;
-import org.assertj.core.api.Assertions;
+import com.logwiki.specialsurveyservice.domain.giveaway.GiveawayType;
+import com.logwiki.specialsurveyservice.domain.survey.Survey;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,28 +22,33 @@ class TargetNumberServiceTest extends IntegrationTestSupport {
     @DisplayName("")
     @Test
     void createTargetNumbers() {
-        Map<Long, Integer> giveaways = new HashMap<>();
-        Long giveawayId1 = 1L;
-        int giveawayCount1 = 3;
-        Long giveawayId2 = 2L;
-        int giveawayCount2 = 5;
-        giveaways.put(giveawayId1, giveawayCount1);
-        giveaways.put(giveawayId2, giveawayCount2);
+        GiveawayAssignServiceRequest giveawayAssignServiceRequest1 = GiveawayAssignServiceRequest.builder()
+                .id(1L)
+                .giveawayType(GiveawayType.COFFEE)
+                .name("스타벅스 아이스 아메리카노")
+                .count(3)
+                .build();
 
-        TargetNumberCreateServiceRequest targetNumberCreateServiceRequest = TargetNumberCreateServiceRequest.create(100,
-                giveaways);
+        GiveawayAssignServiceRequest giveawayAssignServiceRequest2 = GiveawayAssignServiceRequest.builder()
+                .id(2L)
+                .giveawayType(GiveawayType.COFFEE)
+                .name("컴포즈 아이스 아메리카노")
+                .count(5)
+                .build();
 
-        Map<Integer, Long> targetNumbers = targetNumberService.createTargetNumbers(targetNumberCreateServiceRequest);
+        List<GiveawayAssignServiceRequest> giveawayAssignServiceRequests = List.of(giveawayAssignServiceRequest1, giveawayAssignServiceRequest2);
+        int closedHeadCount = 100;
+        Survey survey = Survey.builder()
+                .title("설문조사 제목")
+                .build();
+        TargetNumberCreateServiceRequest targetNumbers = TargetNumberCreateServiceRequest.create(
+                closedHeadCount, giveawayAssignServiceRequests, survey);
 
-        Assertions.assertThat(targetNumbers.size()).isEqualTo(giveawayCount1 + giveawayCount2);
-
-        Assertions.assertThat(targetNumbers.values().stream()
-                        .filter(value -> value.equals(giveawayId1))
-                        .count())
-                .isEqualTo(giveawayCount1);
-        Assertions.assertThat(targetNumbers.values().stream()
-                        .filter(value -> value.equals(giveawayId2))
-                        .count())
-                .isEqualTo(giveawayCount2);
+        assertThat(targetNumbers.getClosedHeadCount()).isEqualTo(closedHeadCount);
+        assertThat(targetNumbers.getGiveaways().size()).isEqualTo(2);
+        assertThat(targetNumbers.getGiveaways().values().stream()
+                .mapToInt(Integer::intValue)
+                .sum())
+                .isEqualTo(giveawayAssignServiceRequest1.getCount() + giveawayAssignServiceRequest2.getCount());
     }
 }
