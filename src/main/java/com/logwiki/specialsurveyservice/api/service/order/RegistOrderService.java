2269980de634c1387e850697015f7cd531a430f1
KWON SEONGEUN <kss4037@gmail.com>
2023-07-26 12:08:01 +0900

    :ambulance: 결제금액을 계산할떄 상품가격을 DB에서 받아오도록 변경

--- src/main/java/com/logwiki/specialsurveyservice/api/service/order/RegistOrderService.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/service/order/RegistOrderService.java
@@ -3,9 +3,13 @@ package com.logwiki.specialsurveyservice.api.service.order;
 import com.logwiki.specialsurveyservice.api.controller.orders.request.OrderCreateRequest;
 import com.logwiki.specialsurveyservice.api.service.order.request.OrderCreateServiceRequest;
 import com.logwiki.specialsurveyservice.api.service.order.response.OrderResponse;
+import com.logwiki.specialsurveyservice.domain.giveaway.Giveaway;
+import com.logwiki.specialsurveyservice.domain.giveaway.GiveawayRepository;
+import com.logwiki.specialsurveyservice.domain.giveaway.GiveawayType;
 import com.logwiki.specialsurveyservice.domain.giveawayPrice.GiveawayPrice;
 import com.logwiki.specialsurveyservice.domain.orders.Orders;
 import com.logwiki.specialsurveyservice.domain.orders.OrdersRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,13 +19,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class RegistOrderService {
 
   private final OrdersRepository orderRepository;
+  private final GiveawayRepository giveawayRepository;
   @Transactional
   public OrderResponse regist(OrderCreateServiceRequest request) {
     int orderAmount = 0;
     for(OrderCreateRequest orderCreateRequest : request.getGiveaways()){
-      orderAmount += GiveawayPrice.valueOf(orderCreateRequest.getGiveawayName()).getPrice()
-        * orderCreateRequest.getGiveawayNumber();
+      Optional<Giveaway> giveaway = giveawayRepository.findGiveawayByName(orderCreateRequest.getGiveawayName());
+      orderAmount += giveaway.get().getPrice() * orderCreateRequest.getGiveawayNumber();
     }
+
+
     Orders order = Orders.create(
         orderAmount
     );
