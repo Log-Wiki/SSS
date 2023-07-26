7dc858956b5577c53b3778675f01ad42ed6aef20
slbin <smpts00@naver.com>
2023-07-26 11:44:12 +0900

    :sparkles: 설문 응답 저장 기능 구현

--- src/main/java/com/logwiki/specialsurveyservice/api/controller/question/QuestionController.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/controller/question/QuestionController.java
@@ -1,18 +1,24 @@
 package com.logwiki.specialsurveyservice.api.controller.question;
 
 import com.logwiki.specialsurveyservice.api.controller.question.request.QuestionAnswerCreateRequest;
+import com.logwiki.specialsurveyservice.api.controller.question.request.QuestionAnswersCreateRequest;
 import com.logwiki.specialsurveyservice.api.controller.question.request.QuestionModifyRequest;
 import com.logwiki.specialsurveyservice.api.service.question.QuestionAnswerService;
 import com.logwiki.specialsurveyservice.api.service.question.QuestionService;
 import com.logwiki.specialsurveyservice.api.utils.ApiResponse;
 import com.logwiki.specialsurveyservice.api.utils.ApiUtils;
 import jakarta.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,20 +30,20 @@ public class QuestionController {
 
     @PutMapping("/question/{id}")
     public ApiResponse<?> questionModify(@PathVariable Long id,
-                                         @Valid @RequestBody QuestionModifyRequest dto) {
+            @Valid @RequestBody QuestionModifyRequest dto) {
         questionService.modifyQuestion(dto.toServiceRequest(id));
         return ApiUtils.success(dto);
     }
 
     @PostMapping("/question/answers")
-    public ApiResponse<?> questionAnswersAdd(@PathVariable Long surveyId,
-                                             @Valid @RequestBody List<QuestionAnswerCreateRequest> dto,
-                                             Authentication authentication) {
+    public ApiResponse<?> questionAnswersAdd(@RequestParam Long surveyId,
+            @Valid @RequestBody QuestionAnswersCreateRequest dto,
+            Authentication authentication) {
         String userEmail = authentication.getName();
-
+        LocalDateTime writeDate = LocalDateTime.now();
         return ApiUtils.success(
-                questionAnswerService.addQuestionAnswer(surveyId, userEmail,
-                        dto.stream()
+                questionAnswerService.addQuestionAnswer(writeDate, surveyId, userEmail,
+                        dto.getAnswers().stream()
                                 .map(QuestionAnswerCreateRequest::toServiceRequest)
                                 .collect(Collectors.toList())));
     }
--- src/main/java/com/logwiki/specialsurveyservice/api/service/question/QuestionAnswerService.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/service/question/QuestionAnswerService.java
@@ -2,17 +2,20 @@ package com.logwiki.specialsurveyservice.api.service.question;
 
 
 import com.logwiki.specialsurveyservice.api.service.question.request.QuestionAnswerCreateServiceRequest;
+import com.logwiki.specialsurveyservice.api.service.question.response.QuestionAnswerCreateServiceResponse;
 import com.logwiki.specialsurveyservice.domain.account.Account;
 import com.logwiki.specialsurveyservice.domain.account.AccountRepository;
 import com.logwiki.specialsurveyservice.domain.question.Question;
 import com.logwiki.specialsurveyservice.domain.question.QuestionRepository;
+import com.logwiki.specialsurveyservice.domain.questionanswer.QuestionAnswer;
 import com.logwiki.specialsurveyservice.domain.questionanswer.QuestionAnswerRepository;
 import com.logwiki.specialsurveyservice.exception.BaseException;
 import jakarta.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @Transactional
@@ -23,19 +26,36 @@ public class QuestionAnswerService {
     private final QuestionRepository questionRepository;
     private final AccountRepository accountRepository;
 
-    public Long addQuestionAnswer(
+    public List<QuestionAnswerCreateServiceResponse> addQuestionAnswer(
+            LocalDateTime writeDate,
             Long surveyId,
             String userEmail,
             List<QuestionAnswerCreateServiceRequest> dto) {
+
         Account account = accountRepository.findOneWithAuthoritiesByEmail(userEmail)
                 .orElseThrow(() -> new BaseException("없는 유저입니다.", 1001));
-        // 설문 만들기전 임시로 문항 체크를 뺌
         List<Question> questions = questionRepository.findBySurveyId(surveyId).orElseThrow(
                 () -> new BaseException("없는 설문입니다.", 2005));
-//        Question question = questionAnswerRepository.findById(dto.getQuestionNumber())
-//                .orElseThrow(() -> new BaseException("없는 문항입니다.", 2004)).getQuestion();
-//        QuestionAnswerCreateServiceResponse(questionAnswerRepository.save(dto.toEntity(null, account)));
-        return 1L;
+
+        List<QuestionAnswerCreateServiceResponse> result = new ArrayList<>();
+
+        for (QuestionAnswerCreateServiceRequest answer : dto) {
+            boolean notFoundQuestion = true;
+            for (Question question : questions) {
+                if (question.getId().equals(answer.getQuestionId())) {
+                    QuestionAnswer questionAnswer = answer.toEntity(question, account);
+                    questionAnswer.setWriteDate(writeDate);
+                    questionAnswerRepository.save(questionAnswer);
+                    result.add(new QuestionAnswerCreateServiceResponse(questionAnswer));
+                    notFoundQuestion = false;
+                }
+            }
+            if (notFoundQuestion) {
+                throw new BaseException("없는 문항에 답변을 할 수 없습니다.", 3000);
+            }
+        }
+
+        return result;
 
     }
 }
