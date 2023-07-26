9ed956ef181ca1ba60765d9b27cd9d4316ec8fc3
amenable <duswo0624@naver.com>
2023-07-26 12:02:01 +0900

    :sparkles: 설문 제작 시 당첨 번호 저장

--- src/main/java/com/logwiki/specialsurveyservice/api/service/targetnumber/TargetNumberService.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/service/targetnumber/TargetNumberService.java
@@ -1,8 +1,15 @@
 package com.logwiki.specialsurveyservice.api.service.targetnumber;
 
 import com.logwiki.specialsurveyservice.api.service.targetnumber.request.TargetNumberCreateServiceRequest;
+import com.logwiki.specialsurveyservice.domain.giveaway.Giveaway;
+import com.logwiki.specialsurveyservice.domain.giveaway.GiveawayRepository;
+import com.logwiki.specialsurveyservice.domain.targetnumber.TargetNumber;
+import com.logwiki.specialsurveyservice.domain.targetnumber.TargetNumberRepository;
+import com.logwiki.specialsurveyservice.exception.BaseException;
 import jakarta.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 import lombok.RequiredArgsConstructor;
@@ -13,13 +20,28 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class TargetNumberService {
 
-    public Map<Integer, Long> createTargetNumbers(TargetNumberCreateServiceRequest request) {
-        Map<Integer, Long> targetNumbers = new HashMap<>();
+    private final GiveawayRepository giveawayRepository;
+
+    private final TargetNumberRepository targetNumberRepository;
+
+    public List<TargetNumber> createTargetNumbers(TargetNumberCreateServiceRequest request) {
         Map<Long, Integer> giveaways = request.getGiveaways();
+        Map<Integer, Long> targetNumbers = getTargetNumbers(request, giveaways);
+
+        List<TargetNumber> targetNumbersForSurvey = getTargetNumbersForSurvey(
+                request, targetNumbers);
+        request.getSurvey().addTargetNumbers(targetNumbersForSurvey);
+
+        return targetNumbersForSurvey;
+    }
+
+    private static Map<Integer, Long> getTargetNumbers(TargetNumberCreateServiceRequest request,
+            Map<Long, Integer> giveaways) {
+        Map<Integer, Long> targetNumbers = new HashMap<>();
         for(Long giveawayId : giveaways.keySet()) {
             int giveawayCount = giveaways.get(giveawayId);
             while(giveawayCount > 0) {
-                int randomNumber = ThreadLocalRandom.current().nextInt(1, request.getHeadCount());
+                int randomNumber = ThreadLocalRandom.current().nextInt(1, request.getClosedHeadCount());
                 if(targetNumbers.containsKey(randomNumber))
                     continue;
 
@@ -28,5 +50,18 @@ public class TargetNumberService {
             }
         }
         return targetNumbers;
+    }
+
+    private List<TargetNumber> getTargetNumbersForSurvey(TargetNumberCreateServiceRequest request,
+            Map<Integer, Long> targetNumbers) {
+        List<TargetNumber> targetNumbersForSurvey = new ArrayList<>();
+        for(int key : targetNumbers.keySet()) {
+            Giveaway giveaway = giveawayRepository.findById(targetNumbers.get(key)).orElseThrow(
+                    () -> new BaseException("설문에 등록할 당첨 상품의 값이 올바르지 않습니다.", 1000));
+            TargetNumber targetNumber = TargetNumber.create(key, request.getSurvey(), giveaway);
+            targetNumbersForSurvey.add(targetNumber);
+            targetNumberRepository.save(targetNumber);
+        }
+        return targetNumbersForSurvey;
     }
 }
