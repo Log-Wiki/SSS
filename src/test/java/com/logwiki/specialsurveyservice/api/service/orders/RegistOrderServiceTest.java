f89ef092c78c386938fbf0530c855836eae23392
KWON SEONGEUN <kss4037@gmail.com>
2023-07-26 17:40:46 +0900

    :white_check_mark: :주문 등록 테스트 추가

---
+++ src/test/java/com/logwiki/specialsurveyservice/api/service/orders/RegistOrderServiceTest.java
@@ -0,0 +1,59 @@
+package com.logwiki.specialsurveyservice.api.service.orders;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.tuple;
+
+import com.logwiki.specialsurveyservice.IntegrationTestSupport;
+import com.logwiki.specialsurveyservice.api.controller.giveaway.request.GiveawayRequest;
+import com.logwiki.specialsurveyservice.api.controller.orders.request.OrderCreateRequest;
+import com.logwiki.specialsurveyservice.api.controller.orders.request.OrderCreateRequest.OrderCreateRequestBuilder;
+import com.logwiki.specialsurveyservice.api.service.giveaway.GiveawayService;
+import com.logwiki.specialsurveyservice.api.service.giveaway.response.GiveawayResponse;
+import com.logwiki.specialsurveyservice.api.service.order.RegistOrderService;
+import com.logwiki.specialsurveyservice.api.service.order.request.OrderCreateServiceRequest;
+import com.logwiki.specialsurveyservice.api.service.order.response.OrderResponse;
+import com.logwiki.specialsurveyservice.domain.giveaway.GiveawayRepository;
+import com.logwiki.specialsurveyservice.domain.giveaway.GiveawayType;
+import com.logwiki.specialsurveyservice.exception.BaseException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+class RegistOrderServiceTest extends IntegrationTestSupport {
+
+    @Autowired
+    RegistOrderService registOrderService;
+
+    @Autowired
+    GiveawayRepository giveawayRepository;
+    @Autowired
+    GiveawayService giveawayService;
+    @DisplayName("사용자ID , {상품명 , 상품금액}리스트로 주문을 등록한다.")
+    @Test
+    void registOrder() {
+        // given
+
+        String userId = "ksr4037@naver.com";
+        List<OrderCreateRequest> giveaways = new ArrayList<>();
+
+        OrderCreateServiceRequest request = OrderCreateServiceRequest.builder().userId(userId).giveaways(giveaways).build();
+        // when
+        OrderResponse saveOrder = registOrderService.regist(request);
+
+        // then
+        assertThat(saveOrder).isNotNull();
+        assertThat(saveOrder)
+                .extracting("orderId", "orderAmount", "isSuccess")
+                .contains("ksr4037@gmail.com", 26000 , true);
+    }
+
+
+}
