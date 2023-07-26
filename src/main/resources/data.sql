b9124f96198a976cf7692d4432a49acf79382e3b
slbin <smpts00@naver.com>
2023-07-26 11:42:20 +0900

    :card_file_box: 테스트를 위한 임시 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,10 +3,16 @@ values ('ROLE_USER');
 insert into authority (type)
 values ('ROLE_ADMIN');
 
-insert into account (email, password, gender, name, phone_number, response_survey_count, create_survey_count, winning_giveaway_count, point, birthday, create_at, modified_at)
-values ('admin@naver.com', '$2a$10$SuhlmaC1Rfyn4F7tc0x8duB9YkDjU1PXY2BXI7xtc08hjsI/RmzF.', 'MALE', '최주인', '010-1111-2222', 0, 0, 0, 0, now(), now(), now());
-insert into account (email, password, gender, name, phone_number, response_survey_count, create_survey_count, winning_giveaway_count, point, birthday, create_at, modified_at)
-values ('user@naver.com', '$2a$10$s493xSE6O4c6kS.TStPjIuiuF0nHXjhemDH3YaSzzIBdFb54EuZNS', 'FEMALE', '김유저', '010-3333-4444', 0, 0, 0, 0, now(), now(), now());
+insert into account (email, password, gender, name, phone_number, response_survey_count,
+                     create_survey_count, winning_giveaway_count, point, birthday, create_at,
+                     modified_at)
+values ('admin@naver.com', '$2a$10$SuhlmaC1Rfyn4F7tc0x8duB9YkDjU1PXY2BXI7xtc08hjsI/RmzF.', 'MALE',
+        '최주인', '010-1111-2222', 0, 0, 0, 0, now(), now(), now());
+insert into account (email, password, gender, name, phone_number, response_survey_count,
+                     create_survey_count, winning_giveaway_count, point, birthday, create_at,
+                     modified_at)
+values ('user@naver.com', '$2a$10$s493xSE6O4c6kS.TStPjIuiuF0nHXjhemDH3YaSzzIBdFb54EuZNS', 'FEMALE',
+        '김유저', '010-3333-4444', 0, 0, 0, 0, now(), now(), now());
 
 insert into account_authority (account_id, authority_id, create_at, modified_at)
 values (1, 1, now(), now());
@@ -36,3 +42,6 @@ values ('SIXTIES');
 
 insert into survey(title, HEAD_COUNT, CLOSED_HEAD_COUNT)
 values ('설문1', 100, 200);
+
+insert into question(content, survey_id)
+values ('문항1', 1);
