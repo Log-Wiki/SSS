18a8003f0c2c78992dc21b8b21decde9c5bb0c39
KWON SEONGEUN <kss4037@gmail.com>
2023-07-26 10:56:19 +0900

    :sparkles: 설문조사 응답 등록시 변동하는 확률과 추가된 응답자를 클라이언트로 보내는 메소드 각각 추가

--- src/main/java/com/logwiki/specialsurveyservice/api/service/sse/SseConnectService.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/service/sse/SseConnectService.java
@@ -3,7 +3,10 @@ package com.logwiki.specialsurveyservice.api.service.sse;
 import com.logwiki.specialsurveyservice.domain.SseEmiters.EmitterRepository;
 import com.logwiki.specialsurveyservice.exception.BaseException;
 import java.io.IOException;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -17,29 +20,58 @@ public class SseConnectService {
         this.emitterRepository = emitterRepository;
     }
 
-    public SseEmitter add(Long login_id , Long survey_id,SseEmitter sseEmitter) {
+    public SseEmitter subscribe(Long login_id , Long survey_id,SseEmitter sseEmitter) {
         String id = survey_id + "_" + login_id + "_" + System.currentTimeMillis();
-        log.info("sseEmitter add Event : addTo {} as {}" , survey_id, id);
         sseEmitter = emitterRepository.save(id , sseEmitter);
-        log.info("sseEmitter now size : {}" , emitterRepository.sseEmitterMap.size());
+        log.info("timeout setting : {}" , sseEmitter.getTimeout());
         try {
             sseEmitter.send(SseEmitter.event()
-                    .name("connect")
-                    .data("connected!"));
+                    .name("connectName1")
+                    .data("connectedMessage1" , MediaType.TEXT_EVENT_STREAM)
+            );
         }
         catch (IOException e) {
-            throw new BaseException("Sse 구독 과정에서 IO 예외발ㄴ생" , 2040);
+            throw new BaseException("Sse 구독 과정에서 IO 예외발생" , 2040);
         }
 
         sseEmitter.onCompletion(() -> {
-            log.info("sseEmitter onCompletion Event : remove {}" , id);
+            log.info("SseEmitter Completion {}" , id);
             emitterRepository.deleteById(id);
         });
         sseEmitter.onTimeout(() -> {
-            log.info("sseEmitter onTimeout : complete {}" , id);
+            log.info("SseEmitter Timeout {}" , id);
             emitterRepository.deleteById(id);
         });
         return sseEmitter;
+    }
+
+    public void refreshSurveyProbability(Long survey_id , String data) {
+        Map<String, SseEmitter> targets = emitterRepository.findAllStartWithById(String.valueOf(survey_id));
+        targets.forEach((key , value) -> {
+            log.info(key);
+            try {
+                value.send(SseEmitter.event()
+                        .id(String.valueOf(survey_id))
+                        .name("확률변동")
+                        .data(data));
+            } catch (IOException e) {
+                throw new BaseException("Sse 이벤트 데이터 송신 과정에서 IO 예외발생" , 2041);
+            }
+        });
+    }
+
+    public void refreshSurveyFinisher(Long survey_id , String data) {
+        Map<String, SseEmitter> targets = emitterRepository.findAllStartWithById(String.valueOf(survey_id));
+        targets.forEach((key , value) -> {
+            try {
+                value.send(SseEmitter.event()
+                        .id(String.valueOf(survey_id))
+                        .name("응답인원추가")
+                        .data(data));
+            } catch (IOException e) {
+                throw new BaseException("Sse 이벤트 데이터 송신 과정에서 IO 예외발생" , 2041);
+            }
+        });
     }
 
 
