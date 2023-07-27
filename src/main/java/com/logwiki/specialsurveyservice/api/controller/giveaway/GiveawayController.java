2a6fb7c84d04154e829aaff00b697fa9af91ee48
Amenable-C <duswo0624@naver.com>
2023-07-28 06:12:29 +0900

    :adhesive_bandage: update 맵핑 수정(put -> patch)

--- src/main/java/com/logwiki/specialsurveyservice/api/controller/giveaway/GiveawayController.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/controller/giveaway/GiveawayController.java
@@ -10,9 +10,9 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +39,7 @@ public class GiveawayController {
         return ApiUtils.success(giveawayService.deleteGiveaway(id));
     }
 
-    @PutMapping("/giveaway/{id}")
+    @PatchMapping("/giveaway/{id}")
     public ApiResponse<GiveawayResponse> updateGiveaway(@PathVariable Long id, @Valid @RequestBody GiveawayRequest giveawayRequest) {
         return ApiUtils.success(giveawayService.updateGiveaway(id, giveawayRequest));
     }
