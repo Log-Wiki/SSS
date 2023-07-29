0970e8aebd57e6da25d5842929ec582a976cbf03
slbin-park <smpts00@naver.com>
2023-07-29 15:02:27 +0900

    :white_check_mark: 없는 설문에 응답했을경우 테스트 코드 작성

--- src/test/java/com/logwiki/specialsurveyservice/api/service/question/QuestionAnswerServiceTest.java
+++ src/test/java/com/logwiki/specialsurveyservice/api/service/question/QuestionAnswerServiceTest.java
@@ -1,12 +1,15 @@
 package com.logwiki.specialsurveyservice.api.service.question;
 
+import com.logwiki.specialsurveyservice.api.service.question.request.QuestionAnswerCreateServiceRequest;
 import com.logwiki.specialsurveyservice.domain.account.Account;
 import com.logwiki.specialsurveyservice.domain.account.AccountRepository;
 import com.logwiki.specialsurveyservice.domain.accountcode.AccountCodeType;
 import com.logwiki.specialsurveyservice.domain.authority.Authority;
 import com.logwiki.specialsurveyservice.domain.authority.AuthorityType;
+import com.logwiki.specialsurveyservice.domain.question.QuestionRepository;
 import com.logwiki.specialsurveyservice.domain.questionanswer.QuestionAnswerRepository;
-import org.junit.jupiter.api.Disabled;
+import com.logwiki.specialsurveyservice.exception.BaseException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +18,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,10 +36,11 @@ class QuestionAnswerServiceTest {
     private AccountRepository accountRepository;
     @Mock
     private QuestionAnswerRepository questionAnswerRepository;
+    @Mock
+    private QuestionRepository questionRepository;
 
-    @DisplayName("없는 문항에 응답을 했을 경우 에러를 반환")
+    @DisplayName("없는 설문에 응답을 했을 경우 에러를 반환")
     @Test
-    @Disabled("설문 추가후 변경")
     void notFoundQuestionThrowError() {
         // given
         Authority authority = Authority.builder()
@@ -53,20 +59,26 @@ class QuestionAnswerServiceTest {
                 LocalDate.of(1997, 6, 24),
                 authorities
         );
-//        QuestionAnswerCreateServiceRequest dto = QuestionAnswerCreateServiceRequest.builder()
-//                .userEmail("asd@naver.com")
-//                .questionNumber(1L)
-//                .build();
+        List<QuestionAnswerCreateServiceRequest> dtoList = new ArrayList<>();
+        QuestionAnswerCreateServiceRequest dto = QuestionAnswerCreateServiceRequest.builder()
+                .questionId(1L)
+                .multipleChoiceAnswer(1L)
+                .build();
+        dtoList.add(dto);
+        LocalDateTime nowDate = LocalDateTime.now();
+        Long surveyId = 1L;
+        String useEmail = "user@naver.com";
 
         // when
-        when(questionAnswerRepository.findById(any()))
+        when(questionRepository.findBySurveyId(any()))
                 .thenReturn(Optional.ofNullable(null));
         when(accountRepository.findOneWithAuthoritiesByEmail(any()))
                 .thenReturn(Optional.ofNullable(account));
 
         // then
-//        Assertions.assertThrows(BaseException.class, () -> {
-//            questionAnswerService.addQuestionAnswer(dto);
-//        });
+        Assertions.assertThrows(BaseException.class, () -> {
+            questionAnswerService.addQuestionAnswer(nowDate, surveyId
+                    , useEmail, dtoList);
+        });
     }
 }
