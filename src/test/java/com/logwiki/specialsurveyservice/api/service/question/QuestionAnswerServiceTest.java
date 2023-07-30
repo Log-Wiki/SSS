b431df80e53ef9ec929dc2f69e42de782b3d84d8
slbin-park <smpts00@naver.com>
2023-07-30 16:44:21 +0900

    :white_check_mark: 문항 개수보다 문항 답변개수가 작을경우 테스트 코드 작성

--- src/test/java/com/logwiki/specialsurveyservice/api/service/question/QuestionAnswerServiceTest.java
+++ src/test/java/com/logwiki/specialsurveyservice/api/service/question/QuestionAnswerServiceTest.java
@@ -3,11 +3,16 @@ package com.logwiki.specialsurveyservice.api.service.question;
 import com.logwiki.specialsurveyservice.api.service.question.request.QuestionAnswerCreateServiceRequest;
 import com.logwiki.specialsurveyservice.domain.account.Account;
 import com.logwiki.specialsurveyservice.domain.account.AccountRepository;
+import com.logwiki.specialsurveyservice.domain.accountcode.AccountCode;
 import com.logwiki.specialsurveyservice.domain.accountcode.AccountCodeType;
 import com.logwiki.specialsurveyservice.domain.authority.Authority;
 import com.logwiki.specialsurveyservice.domain.authority.AuthorityType;
+import com.logwiki.specialsurveyservice.domain.question.Question;
 import com.logwiki.specialsurveyservice.domain.question.QuestionRepository;
 import com.logwiki.specialsurveyservice.domain.questionanswer.QuestionAnswerRepository;
+import com.logwiki.specialsurveyservice.domain.questioncategory.QuestionCategoryType;
+import com.logwiki.specialsurveyservice.domain.surveytarget.SurveyTarget;
+import com.logwiki.specialsurveyservice.domain.surveytarget.SurveyTargetRepository;
 import com.logwiki.specialsurveyservice.exception.BaseException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -39,27 +44,14 @@ class QuestionAnswerServiceTest {
     private QuestionAnswerRepository questionAnswerRepository;
     @Mock
     private QuestionRepository questionRepository;
+    @Mock
+    private SurveyTargetRepository surveyTargetRepository;
 
     @DisplayName("없는 설문에 응답을 했을 경우 에러를 반환")
     @Test
     void notFoundQuestionThrowError() {
         // given
-        Authority authority = Authority.builder()
-                .type(AuthorityType.ROLE_USER)
-                .build();
-        List<Authority> authorities = List.of(authority);
-
-        String email = "duswo0624@naver.com";
-        Account account = Account.create(
-                email,
-                "1234",
-                AccountCodeType.MAN,
-                AccountCodeType.TWENTIES,
-                "최연재",
-                "010-1234-5678",
-                LocalDate.of(1997, 6, 24),
-                authorities
-        );
+        Account account = getAccount();
         List<QuestionAnswerCreateServiceRequest> dtoList = new ArrayList<>();
         QuestionAnswerCreateServiceRequest dto = QuestionAnswerCreateServiceRequest.builder()
                 .questionId(1L)
@@ -83,7 +75,7 @@ class QuestionAnswerServiceTest {
         });
     }
 
-    @DisplayName("모든 질문에 답을하지 않을경우 에러를 반환한다.")
+    @DisplayName("문항의 개수보다 문항 답변 개수가 작을경우 에러를 반환.")
     @Test
     void needAllAnswer() {
         // given
@@ -91,21 +83,52 @@ class QuestionAnswerServiceTest {
         Account account = getAccount();
 
         List<QuestionAnswerCreateServiceRequest> dtoList = new ArrayList<>();
-        QuestionAnswerCreateServiceRequest dto = QuestionAnswerCreateServiceRequest.builder()
-                .questionId(1L)
-                .multipleChoiceAnswer(1L)
+        for (int i = 0; i < 5; i++) {
+            QuestionAnswerCreateServiceRequest dto = QuestionAnswerCreateServiceRequest.builder()
+                    .questionId(1L)
+                    .multipleChoiceAnswer(1L)
+                    .build();
+            dtoList.add(dto);
+        }
+        List<Question> questions = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            Question question = Question.builder()
+                    .questionNumber(Long.parseLong(String.valueOf(i)))
+                    .content("문제" + String.valueOf(i))
+                    .type(QuestionCategoryType.MULTIPLE_CHOICE)
+                    .multipleChoice(new ArrayList<>())
+                    .build();
+            questions.add(question);
+        }
+
+        List<SurveyTarget> surveyTargets = new ArrayList<>();
+        AccountCode accountCode1 = AccountCode.builder()
+                .type(AccountCodeType.MAN)
                 .build();
-        dtoList.add(dto);
-        
+        SurveyTarget surveyTarget1 = SurveyTarget.builder()
+                .accountCode(accountCode1)
+                .build();
+        AccountCode accountCode2 = AccountCode.builder()
+                .type(AccountCodeType.TWENTIES)
+                .build();
+        SurveyTarget surveyTarget2 = SurveyTarget.builder()
+                .accountCode(accountCode2)
+                .build();
+        surveyTargets.add(surveyTarget1);
+        surveyTargets.add(surveyTarget2);
+
+
         LocalDateTime nowDate = LocalDateTime.now();
         Long surveyId = 1L;
         String useEmail = "user@naver.com";
 
         // when
         when(questionRepository.findBySurveyId(any()))
-                .thenReturn(Optional.ofNullable(null));
+                .thenReturn(Optional.of(questions));
         when(accountRepository.findOneWithAuthoritiesByEmail(any()))
                 .thenReturn(Optional.ofNullable(account));
+        when(surveyTargetRepository.findSurveyTargetBySurvey_Id(any()))
+                .thenReturn(surveyTargets);
 
         // then
         Assertions.assertThrows(BaseException.class, () -> {
