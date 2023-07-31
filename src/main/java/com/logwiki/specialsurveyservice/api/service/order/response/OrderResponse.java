6b187f6a59d0b314f7b9eaa2ba596f7c2b811447
KWON SEONGEUN <kss4037@gmail.com>
2023-07-31 14:01:44 +0900

    :ambulance: 주문 응답에 IAMPORT 가맹점 아이디 추가

--- src/main/java/com/logwiki/specialsurveyservice/api/service/order/response/OrderResponse.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/service/order/response/OrderResponse.java
@@ -1,6 +1,7 @@
 package com.logwiki.specialsurveyservice.api.service.order.response;
 
 import com.logwiki.specialsurveyservice.domain.orders.Orders;
+import com.logwiki.specialsurveyservice.domain.payment.IamportApiConstant;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,16 +17,21 @@ public class OrderResponse {
     private final Integer orderAmount;
     @NotNull(message = "주문 결제여부는 필수 입니다")
     private final Boolean isVerificated;
+    @NotNull(message = "가맹정ID는 필수 입니다.")
+    private final String storeId;
 
     @Builder
-    private OrderResponse(String orderId, Integer orderAmount, Boolean isVerificated) {
+    private OrderResponse(String orderId, Integer orderAmount, Boolean isVerificated,
+            String storeId) {
         this.orderId = orderId;
         this.orderAmount = orderAmount;
         this.isVerificated = isVerificated;
+        this.storeId = storeId;
     }
 
     public static OrderResponse from(Orders order) {
         return OrderResponse.builder().orderId(order.getOrderId())
-                .orderAmount(order.getOrderAmount()).isVerificated(order.getIsVerificated()).build();
+                .orderAmount(order.getOrderAmount()).isVerificated(order.getIsVerificated()).storeId(
+                        IamportApiConstant.StoreKey).build();
     }
 }
