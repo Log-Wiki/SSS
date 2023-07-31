5f87287b7d194f906615943eb86333bd89f1d76c
slbin-park <smpts00@naver.com>
2023-07-31 22:03:46 +0900

    :sparkles: 설문 아이디로 저장된 스케줄러 검색 추가

--- src/main/java/com/logwiki/specialsurveyservice/domain/schedule/ScheduleRepository.java
+++ src/main/java/com/logwiki/specialsurveyservice/domain/schedule/ScheduleRepository.java
@@ -3,10 +3,14 @@ package com.logwiki.specialsurveyservice.domain.schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     Optional<Schedule> findScheduleByJobGroupAndJobName(String jobGroup, String jobName);
+
+    Optional<List<Schedule>> findScheduleBySurveyId(Long surveyId);
+    
 }
