62f6de9950bfcc6f839b42c3735c3dcc3529b482
slbin-park <smpts00@naver.com>
2023-07-31 23:38:05 +0900

    :sparkles: 생성자 빌더패턴으로 추가

--- src/main/java/com/logwiki/specialsurveyservice/api/controller/schedule/request/ScheduleCreateRequest.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/controller/schedule/request/ScheduleCreateRequest.java
@@ -1,5 +1,6 @@
 package com.logwiki.specialsurveyservice.api.controller.schedule.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,14 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 public class ScheduleCreateRequest {
+
     private Long surveyId;
+
     private LocalDateTime startTime;
+
+    @Builder
+    public ScheduleCreateRequest(Long surveyId, LocalDateTime startTime) {
+        this.surveyId = surveyId;
+        this.startTime = startTime;
+    }
 }
