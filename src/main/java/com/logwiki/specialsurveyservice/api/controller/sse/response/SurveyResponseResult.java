4d45387e4e410ac455273fc00004d03844589d19
KWON SEONGEUN <kss4037@gmail.com>
2023-07-31 14:48:20 +0900

    :ambulance: Validation 추가

--- src/main/java/com/logwiki/specialsurveyservice/api/controller/sse/response/SurveyResponseResult.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/controller/sse/response/SurveyResponseResult.java
@@ -1,5 +1,6 @@
 package com.logwiki.specialsurveyservice.api.controller.sse.response;
 
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,9 +9,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class SurveyResponseResult {
+    @NotNull(message = "응답 시간은 필수입니다.")
     private LocalDateTime answerTime;
+    @NotNull(message = "응답자 이름은 필수입니다.")
     private String name;
+    @NotNull(message = "응답 상품은 필수입니다.")
     private String giveAwayName;
+    @NotNull(message = "상품 당청 여부는 필수입니다.")
     private Boolean isWin;
 
     @Builder
--- src/main/java/com/logwiki/specialsurveyservice/api/service/survey/response/SurveyDetailgetServiceResponse.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/service/survey/response/SurveyDetailgetServiceResponse.java
@@ -4,6 +4,7 @@ import com.logwiki.specialsurveyservice.api.controller.sse.response.SurveyRespon
 import com.logwiki.specialsurveyservice.domain.giveaway.Giveaway;
 import com.logwiki.specialsurveyservice.domain.surveycategory.SurveyCategory;
 import com.logwiki.specialsurveyservice.domain.surveycategory.SurveyCategoryType;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
@@ -12,30 +13,31 @@ import lombok.Getter;
 @Getter
 public class SurveyDetailgetServiceResponse {
 
+    @NotNull(message = "설문 상세정보의 제목은 필수입니다.")
     private final String title;
-
+    @NotNull(message = "설문 상세정보의 시작시간은 필수입니다.")
     private final LocalDateTime startTime;
-
+    @NotNull(message = "설문 상세정보의 끝나는시간은 필수입니다.")
     private final LocalDateTime endTime;
-
+    @NotNull(message = "설문 상세정보의 현재 진행인원 필수입니다.")
     private final int headCount;
-
+    @NotNull(message = "설문 상세정보의 마감인원 필수입니다.")
     private final int closedHeadCount;
-
+    @NotNull(message = "설문 상세정보의 작성자ID은 필수입니다.")
     private final Long writer;
-
+    @NotNull(message = "설문 상세정보의 작성자이름은 필수입니다.")
     private final String writerName;
-
+    @NotNull(message = "설문 상세정보의 설문카테고리타입은 필수입니다.")
     private final SurveyCategoryType surveyCategoryType;
-
+    @NotNull(message = "설문 상세정보의 상품리스트는 필수입니다.")
     private final List<Giveaway> giveaways;
-
+    @NotNull(message = "설문 상세정보의 당청확률 필수입니다.")
     private final double winRate;
-
+    @NotNull(message = "설문 상세정보의 질문 문항수 필수입니다.")
     private final int questionCount;
-
+    @NotNull(message = "설문 상세정보의 예상시간 필수입니다.")
     private final double estimateTime;
-
+    @NotNull(message = "설문 상세정보의 응답결과내역은 필수입니다.")
     private final List<SurveyResponseResult> surveyResponseResults;
 
     @Builder
