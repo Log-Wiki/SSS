9199e8518731e04f364c092432f3227db1a1571f
slbin <smpts00@naver.com>
2023-07-31 17:41:52 +0900

    :sparkles: jobGroup , jobName 으로 스케줄링 검색 기능 구현

--- src/main/java/com/logwiki/specialsurveyservice/domain/schedule/ScheduleRepository.java
+++ src/main/java/com/logwiki/specialsurveyservice/domain/schedule/ScheduleRepository.java
@@ -3,6 +3,10 @@ package com.logwiki.specialsurveyservice.domain.schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    Optional<Schedule> findScheduleByJobGroupAndJobName(String jobGroup, String jobName);
 }
