992a58c544fc0f59df29e19d778447133a2494cb
amenable <duswo0624@naver.com>
2023-08-02 12:00:14 +0900

    :adhesive_bandage: 필드값 수정 (isWin -> win)

--- src/main/java/com/logwiki/specialsurveyservice/domain/surveyresult/SurveyResult.java
+++ src/main/java/com/logwiki/specialsurveyservice/domain/surveyresult/SurveyResult.java
@@ -20,7 +20,7 @@ public class SurveyResult extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Boolean isWin;
+    private boolean win;
 
     private LocalDateTime endTime;
 
@@ -37,8 +37,8 @@ public class SurveyResult extends BaseEntity {
     private Account account;
 
     @Builder
-    private SurveyResult(Boolean isWin, LocalDateTime endTime, int submitOrder, boolean userCheck, Survey survey, Account account) {
-        this.isWin = isWin;
+    private SurveyResult(Boolean win, LocalDateTime endTime, int submitOrder, boolean userCheck, Survey survey, Account account) {
+        this.win = win;
         this.endTime = endTime;
         this.submitOrder = submitOrder;
         this.userCheck = userCheck;
@@ -48,7 +48,7 @@ public class SurveyResult extends BaseEntity {
 
     public static SurveyResult create(Boolean isWin, LocalDateTime endTime, int submitOrder, Survey survey, Account account) {
         return SurveyResult.builder()
-                .isWin(isWin)
+                .win(isWin)
                 .endTime(endTime)
                 .submitOrder(submitOrder)
                 .userCheck(false)
