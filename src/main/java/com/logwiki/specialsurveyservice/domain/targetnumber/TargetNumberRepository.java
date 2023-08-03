32a9358fe82cb39b9b75c7c9ba95e51e9a611f8f
seongeunkwon <kss4037@gmail.com>
2023-08-04 00:12:00 +0900

    :tada: 설문과 당청번호를 사용하여 타겟넘버 찾기 정의

--- src/main/java/com/logwiki/specialsurveyservice/domain/targetnumber/TargetNumberRepository.java
+++ src/main/java/com/logwiki/specialsurveyservice/domain/targetnumber/TargetNumberRepository.java
@@ -1,8 +1,13 @@
 package com.logwiki.specialsurveyservice.domain.targetnumber;
 
+서import com.logwiki.specialsurveyservice.domain.survey.Survey;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface TargetNumberRepository extends JpaRepository<TargetNumber, Long> {
 
     TargetNumber findTargetNumberByNumberAndSurvey_Id(int targetNumber, Long surveyId);
+
+    Optional<TargetNumber> findFirstBySurveyAndNumber(Survey survey, int number);
 }
