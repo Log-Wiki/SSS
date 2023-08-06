066640a01667707859282b3617a04942e7d32005
slbin-park <smpts00@naver.com>
2023-08-07 02:25:38 +0900

    :sparkles: 연계문항 필수로 받도록 수정 - 연계문항이 없어도 디폴트값을 받도록 수정

--- src/main/java/com/logwiki/specialsurveyservice/api/controller/question/request/MultipleChoiceCreateRequest.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/controller/question/request/MultipleChoiceCreateRequest.java
@@ -2,6 +2,7 @@ package com.logwiki.specialsurveyservice.api.controller.question.request;
 
 import com.logwiki.specialsurveyservice.api.service.question.request.MultipleChoiceCreateServiceRequest;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ public class MultipleChoiceCreateRequest {
     @NotEmpty(message = "문제 보기 내용은 필수입니다.")
     private String content;
 
+    @NotNull(message = "연계 문항 번호가 필요합니다 없는 경우 디폴트값 이 필요합니다.")
     private Long linkNumber;
 
     @Builder
