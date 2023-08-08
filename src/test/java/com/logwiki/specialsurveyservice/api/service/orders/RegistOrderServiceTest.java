1df46aa8f56af3b5d9bf02bd5db7a79da0c1eb3f
KWON SEONGEUN <kss4037@gmail.com>
2023-08-08 15:14:28 +0900

    :ambulance: 주문등록 서비스 테스트 수정

--- src/test/java/com/logwiki/specialsurveyservice/api/service/orders/RegistOrderServiceTest.java
+++ src/test/java/com/logwiki/specialsurveyservice/api/service/orders/RegistOrderServiceTest.java
@@ -4,6 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.logwiki.specialsurveyservice.IntegrationTestSupport;
+import com.logwiki.specialsurveyservice.api.service.account.AccountService;
+import com.logwiki.specialsurveyservice.api.service.account.request.AccountCreateServiceRequest;
+import com.logwiki.specialsurveyservice.domain.accountcode.AccountCode;
+import com.logwiki.specialsurveyservice.domain.accountcode.AccountCodeRepository;
+import com.logwiki.specialsurveyservice.domain.accountcode.AccountCodeType;
+import com.logwiki.specialsurveyservice.domain.authority.Authority;
+import com.logwiki.specialsurveyservice.domain.authority.AuthorityRepository;
+import com.logwiki.specialsurveyservice.domain.authority.AuthorityType;
 import com.logwiki.specialsurveyservice.domain.orders.OrderProductElement;
 import com.logwiki.specialsurveyservice.api.service.giveaway.GiveawayService;
 import com.logwiki.specialsurveyservice.api.service.order.RegistOrderService;
@@ -13,11 +21,14 @@ import com.logwiki.specialsurveyservice.domain.giveaway.Giveaway;
 import com.logwiki.specialsurveyservice.domain.giveaway.GiveawayRepository;
 import com.logwiki.specialsurveyservice.domain.giveaway.GiveawayType;
 import com.logwiki.specialsurveyservice.exception.BaseException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
@@ -30,19 +41,42 @@ class RegistOrderServiceTest extends IntegrationTestSupport {
     GiveawayRepository giveawayRepository;
     @Autowired
     GiveawayService giveawayService;
+    @Autowired
+    AccountService accountService;
+    @Autowired
+    AuthorityRepository authorityRepository;
+    @Autowired
+    AccountCodeRepository accountCodeRepository;
+
+    @BeforeEach
+    void setUp() {
+        setAuthority();
+        setAccountCode();
+    }
     @DisplayName("사용자ID , {상품명 , 상품금액}리스트로 주문을 등록한다.")
+    @WithMockUser(username = "duswo0624@naver.com")
     @Test
     void registOrder() {
         // given
+        String email = "duswo0624@naver.com";
+        AccountCreateServiceRequest accountCreateServiceRequest = AccountCreateServiceRequest.builder()
+                .email(email)
+                .password("1234")
+                .gender(AccountCodeType.MAN)
+                .age(AccountCodeType.TWENTIES)
+                .name("최연재")
+                .phoneNumber("010-1234-5678")
+                .birthday(LocalDate.of(1997, 6, 24))
+                .build();
+        accountService.signup(accountCreateServiceRequest);
 
-        String userId = "ksr4037@naver.com";
         List<OrderProductElement> giveaways = new ArrayList<>();
         giveaways.add(new OrderProductElement("컴포즈커피",3));
         giveaways.add(new OrderProductElement("BBQ후라이드치킨",2));
         giveawayRepository.save(new Giveaway(GiveawayType.COFFEE,"컴포즈커피",1300));
         giveawayRepository.save(new Giveaway(GiveawayType.CHICKEN,"BBQ후라이드치킨",20000));
         
-        OrderCreateServiceRequest request = OrderCreateServiceRequest.builder().userId(userId).giveaways(giveaways).build();
+        OrderCreateServiceRequest request = OrderCreateServiceRequest.builder().giveaways(giveaways).build();
         // when
         OrderResponse saveOrder = registOrderService.createOrder(request);
 
@@ -55,18 +89,29 @@ class RegistOrderServiceTest extends IntegrationTestSupport {
     }
 
     @DisplayName("주문 금액이 0원이어서는 안된다.")
+    @WithMockUser(username = "duswo0624@naver.com")
     @Test
     void registOrderWithNoMoney() {
         // given
+        String email = "duswo0624@naver.com";
+        AccountCreateServiceRequest accountCreateServiceRequest = AccountCreateServiceRequest.builder()
+                .email(email)
+                .password("1234")
+                .gender(AccountCodeType.MAN)
+                .age(AccountCodeType.TWENTIES)
+                .name("최연재")
+                .phoneNumber("010-1234-5678")
+                .birthday(LocalDate.of(1997, 6, 24))
+                .build();
+        accountService.signup(accountCreateServiceRequest);
 
-        String userId = "ksr4037@naver.com";
         List<OrderProductElement> giveaways = new ArrayList<>();
         giveaways.add(new OrderProductElement("컴포즈커피",0));
         giveaways.add(new OrderProductElement("BBQ후라이드치킨",0));
         giveawayRepository.save(new Giveaway(GiveawayType.COFFEE,"컴포즈커피",1300));
         giveawayRepository.save(new Giveaway(GiveawayType.CHICKEN,"BBQ후라이드치킨",20000));
 
-        OrderCreateServiceRequest request = OrderCreateServiceRequest.builder().userId(userId).giveaways(giveaways).build();
+        OrderCreateServiceRequest request = OrderCreateServiceRequest.builder().giveaways(giveaways).build();
         // when // then
         assertThatThrownBy(() ->registOrderService.createOrder(request))
                 .isInstanceOf(BaseException.class)
@@ -75,22 +120,50 @@ class RegistOrderServiceTest extends IntegrationTestSupport {
     }
 
     @DisplayName("입력받은 주문상품이 존재하지 않습니다.")
+    @WithMockUser(username = "duswo0624@naver.com")
     @Test
     void registOrderWithNoProduct() {
         // given
+        String email = "duswo0624@naver.com";
+        AccountCreateServiceRequest accountCreateServiceRequest = AccountCreateServiceRequest.builder()
+                .email(email)
+                .password("1234")
+                .gender(AccountCodeType.MAN)
+                .age(AccountCodeType.TWENTIES)
+                .name("최연재")
+                .phoneNumber("010-1234-5678")
+                .birthday(LocalDate.of(1997, 6, 24))
+                .build();
+        accountService.signup(accountCreateServiceRequest);
 
-        String userId = "ksr4037@naver.com";
         List<OrderProductElement> giveaways = new ArrayList<>();
         giveaways.add(new OrderProductElement("컴포즈커피",3));
         giveaways.add(new OrderProductElement("BBQ후라이드치킨",2));
         giveawayRepository.save(new Giveaway(GiveawayType.CHICKEN,"BBQ후라이드치킨",20000));
 
-        OrderCreateServiceRequest request = OrderCreateServiceRequest.builder().userId(userId).giveaways(giveaways).build();
+        OrderCreateServiceRequest request = OrderCreateServiceRequest.builder().giveaways(giveaways).build();
         // when // then
         assertThatThrownBy(() ->registOrderService.createOrder(request))
                 .isInstanceOf(BaseException.class)
                 .hasMessage("주문 상품이 존재하지 않습니다.");
 
+    }
+
+    private void setAuthority() {
+        Authority userAuthority = Authority.builder()
+                .type(AuthorityType.ROLE_USER)
+                .build();
+        authorityRepository.save(userAuthority);
+    }
+    private void setAccountCode() {
+        List<AccountCodeType> accountCodeTypes = List.of(AccountCodeType.MAN, AccountCodeType.WOMAN, AccountCodeType.UNDER_TEENS,
+                AccountCodeType.TEENS, AccountCodeType.TWENTIES, AccountCodeType.THIRTIES,
+                AccountCodeType.FORTIES, AccountCodeType.FIFTIES, AccountCodeType.SIXTIES);
+        for (AccountCodeType accountCodeType : accountCodeTypes) {
+            accountCodeRepository.save(AccountCode.builder()
+                    .type(accountCodeType)
+                    .build());
+        }
     }
 
 
