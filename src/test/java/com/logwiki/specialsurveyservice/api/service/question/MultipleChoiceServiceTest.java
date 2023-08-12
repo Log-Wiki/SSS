64ea1d5f6540a21cc986c5443ba93c9d51c7d5bc
Amenable-C <duswo0624@naver.com>
2023-08-12 14:26:55 +0900

    :white_check_mark: 질문 관련 테스트 작성

--- src/test/java/com/logwiki/specialsurveyservice/api/service/question/MultipleChoiceServiceTest.java
+++ src/test/java/com/logwiki/specialsurveyservice/api/service/question/MultipleChoiceServiceTest.java
@@ -1,43 +1,57 @@
 package com.logwiki.specialsurveyservice.api.service.question;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.logwiki.specialsurveyservice.IntegrationTestSupport;
+import com.logwiki.specialsurveyservice.api.service.question.response.MultipleChoiceResponse;
 import com.logwiki.specialsurveyservice.domain.multiplechoice.MultipleChoice;
 import com.logwiki.specialsurveyservice.domain.multiplechoice.MultipleChoiceRepository;
 import com.logwiki.specialsurveyservice.exception.BaseException;
-import java.util.Optional;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
+class MultipleChoiceServiceTest extends IntegrationTestSupport {
 
-@ExtendWith(MockitoExtension.class)
-class MultipleChoiceServiceTest {
+    @Autowired
+    private MultipleChoiceService multipleChoiceService;
+    @Autowired
+    private MultipleChoiceRepository multipleChoiceRepository;
 
-  @InjectMocks
-  private MultipleChoiceService multipleChoiceService;
+    @DisplayName("다중선택ID를 이용하여 다중선택을 조회한다.")
+    @Test
+    void findMultipleChoice() {
+        // given
+        String multipleChoiceContent = "다중선택 내용";
+        Long multipleChoiceLinkNumber = 1L;
+        MultipleChoice multipleChoice = MultipleChoice
+                .builder()
+                .content(multipleChoiceContent)
+                .linkNumber(multipleChoiceLinkNumber)
+                .build();
 
-  @Mock
-  private MultipleChoiceRepository multipleChoiceRepository;
+        MultipleChoice saveMultipleChoice = multipleChoiceRepository.save(multipleChoice);
 
-  @DisplayName("문항 보기를 찾지 못할시 에러 반환")
-  @Test
-  void notFindMultipleChoiceThrowError() {
-    // given
-    MultipleChoice multipleChoice = null;
-    Long id = 1L;
+        // when
+        MultipleChoiceResponse multipleChoiceResponse = multipleChoiceService.findMultipleChoice(saveMultipleChoice.getId());
 
-    // when
-    when(multipleChoiceRepository.findById(any())).thenReturn(Optional.ofNullable(multipleChoice));
+        // then
+        assertThat(multipleChoiceResponse.getContent()).isEqualTo(multipleChoiceContent);
+        assertThat(multipleChoiceResponse.getLinkNumber()).isEqualTo(multipleChoiceLinkNumber);
+    }
 
-    // then
-    Assertions.assertThrows(BaseException.class, () -> {
-      multipleChoiceService.findMultipleChoice(id);
-    });
-  }
+    @DisplayName("문항 보기를 찾지 못할시 에러 반환")
+    @Test
+    void notFindMultipleChoiceThrowError() {
+        // given
+        Long invalidMultipleChoiceId = 1L;
+
+        // when // then
+        Assertions.assertThatThrownBy(() -> multipleChoiceService.findMultipleChoice(invalidMultipleChoiceId))
+                .isInstanceOf(BaseException.class)
+                .hasMessage("문항 보기 정보를 찾을 수 없습니다.");
+    }
 }
