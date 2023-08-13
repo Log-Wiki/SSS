d85ca83e8a311e10117f0f6b9f3bff43f11ef5f9
Amenable-C <duswo0624@naver.com>
2023-08-14 06:40:24 +0900

    :sparkles: 당첨자 조회 기능 추가

--- src/main/java/com/logwiki/specialsurveyservice/api/controller/surveyresult/SurveyResultController.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/controller/surveyresult/SurveyResultController.java
@@ -1,8 +1,10 @@
 package com.logwiki.specialsurveyservice.api.controller.surveyresult;
 
 import com.logwiki.specialsurveyservice.api.service.surveyresult.SurveyResultService;
+import com.logwiki.specialsurveyservice.api.service.surveyresult.response.WinningAccountResponse;
 import com.logwiki.specialsurveyservice.api.utils.ApiResponse;
 import com.logwiki.specialsurveyservice.api.utils.ApiUtils;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,5 +23,10 @@ public class SurveyResultController {
     @PatchMapping("/user/survey-result/check/{surveyId}")
     public ApiResponse<?> patchSurveyResult(@PathVariable Long surveyId) {
         return ApiUtils.success(surveyResultService.patchSurveyResult(surveyId));
+    }
+
+    @GetMapping("/survey-result/users/{surveyId}")
+    public ApiResponse<List<WinningAccountResponse>> getWinningUsers(@PathVariable Long surveyId) {
+        return ApiUtils.success(surveyResultService.getWinningUsers(surveyId));
     }
 }
