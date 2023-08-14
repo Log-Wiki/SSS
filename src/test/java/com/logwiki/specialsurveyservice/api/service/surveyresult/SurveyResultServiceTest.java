786185e4f506a860aa1768c0ff63ba00311c7493
amenable <duswo0624@naver.com>
2023-08-14 10:07:52 +0900

    :white_check_mark: 당첨자 조회 Business Layer 테스트 작성

--- src/test/java/com/logwiki/specialsurveyservice/api/service/surveyresult/SurveyResultServiceTest.java
+++ src/test/java/com/logwiki/specialsurveyservice/api/service/surveyresult/SurveyResultServiceTest.java
@@ -1,12 +1,23 @@
 package com.logwiki.specialsurveyservice.api.service.surveyresult;
 
 import com.logwiki.specialsurveyservice.IntegrationTestSupport;
+import com.logwiki.specialsurveyservice.api.controller.giveaway.request.GiveawayRequest;
 import com.logwiki.specialsurveyservice.api.service.account.AccountService;
 import com.logwiki.specialsurveyservice.api.service.account.request.AccountCreateServiceRequest;
 import com.logwiki.specialsurveyservice.api.service.giveaway.GiveawayService;
 import com.logwiki.specialsurveyservice.api.service.giveaway.response.MyGiveawayResponse;
+import com.logwiki.specialsurveyservice.api.service.question.QuestionAnswerService;
+import com.logwiki.specialsurveyservice.api.service.question.request.QuestionAnswerCreateServiceRequest;
+import com.logwiki.specialsurveyservice.api.service.question.request.QuestionCreateServiceRequest;
+import com.logwiki.specialsurveyservice.api.service.survey.SurveyService;
+import com.logwiki.specialsurveyservice.api.service.survey.request.GiveawayAssignServiceRequest;
+import com.logwiki.specialsurveyservice.api.service.survey.request.SurveyCreateServiceRequest;
+import com.logwiki.specialsurveyservice.api.service.survey.response.SurveyResponse;
+import com.logwiki.specialsurveyservice.api.service.surveyresult.response.WinningAccountResponse;
 import com.logwiki.specialsurveyservice.domain.account.Account;
 import com.logwiki.specialsurveyservice.domain.account.AccountRepository;
+import com.logwiki.specialsurveyservice.domain.accountcode.AccountCode;
+import com.logwiki.specialsurveyservice.domain.accountcode.AccountCodeRepository;
 import com.logwiki.specialsurveyservice.domain.accountcode.AccountCodeType;
 import com.logwiki.specialsurveyservice.domain.authority.Authority;
 import com.logwiki.specialsurveyservice.domain.authority.AuthorityRepository;
@@ -14,15 +25,16 @@ import com.logwiki.specialsurveyservice.domain.authority.AuthorityType;
 import com.logwiki.specialsurveyservice.domain.giveaway.Giveaway;
 import com.logwiki.specialsurveyservice.domain.giveaway.GiveawayRepository;
 import com.logwiki.specialsurveyservice.domain.giveaway.GiveawayType;
+import com.logwiki.specialsurveyservice.domain.questioncategory.QuestionCategoryType;
 import com.logwiki.specialsurveyservice.domain.survey.Survey;
 import com.logwiki.specialsurveyservice.domain.survey.SurveyRepository;
 import com.logwiki.specialsurveyservice.domain.surveycategory.SurveyCategory;
+import com.logwiki.specialsurveyservice.domain.surveycategory.SurveyCategoryRepository;
 import com.logwiki.specialsurveyservice.domain.surveycategory.SurveyCategoryType;
 import com.logwiki.specialsurveyservice.domain.surveygiveaway.SurveyGiveaway;
 import com.logwiki.specialsurveyservice.domain.surveyresult.SurveyResult;
 import com.logwiki.specialsurveyservice.domain.surveyresult.SurveyResultRepository;
 import com.logwiki.specialsurveyservice.domain.targetnumber.TargetNumber;
-import com.logwiki.specialsurveyservice.domain.targetnumber.TargetNumberRepository;
 import com.logwiki.specialsurveyservice.exception.BaseException;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,11 +66,17 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
     @Autowired
     private SurveyResultRepository surveyResultRepository;
     @Autowired
-    private TargetNumberRepository targetNumberRepository;
-    @Autowired
     private GiveawayService giveawayService;
     @Autowired
     private AccountRepository accountRepository;
+    @Autowired
+    private SurveyService surveyService;
+    @Autowired
+    private QuestionAnswerService questionAnswerService;
+    @Autowired
+    private AccountCodeRepository accountCodeRepository;
+    @Autowired
+    private SurveyCategoryRepository surveyCategoryRepository;
 
     @DisplayName("설문 번호와 설문 응답 시간을 이용하여 설문 응답 결과를 제출한다.")
     @WithMockUser(username = "duswo0624@naver.com")
@@ -66,7 +84,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
     void addSubmitResult() {
         // given
         setAuthority();
-        saveAccountYJ();
+        saveAccountYJ("duswo0624@naver.com");
 
         SurveyCategory surveyCategory = SurveyCategory.builder()
                 .type(SurveyCategoryType.NORMAL)
@@ -100,7 +118,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
     void cannotAddSubmitResultWithClosedSurvey() {
         // given
         setAuthority();
-        saveAccountYJ();
+        saveAccountYJ("duswo0624@naver.com");
 
         SurveyCategory surveyCategory = SurveyCategory.builder()
                 .type(SurveyCategoryType.NORMAL)
@@ -137,7 +155,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
     void cannotAddSubmitResultToAlreadyAnsweredSurvey() {
         // given
         setAuthority();
-        saveAccountYJ();
+        saveAccountYJ("duswo0624@naver.com");
 
         SurveyCategory surveyCategory = SurveyCategory.builder()
                 .type(SurveyCategoryType.NORMAL)
@@ -167,7 +185,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
     void changeWinningGiveawayOfAccountCountAndWinOfSurveyResult() {
         // given
         setAuthority();
-        saveAccountYJ();
+        saveAccountYJ("duswo0624@naver.com");
 
         SurveyCategory surveyCategory = SurveyCategory.builder()
                 .type(SurveyCategoryType.INSTANT_WIN)
@@ -200,7 +218,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
     void cannotAddSubmitResultWithInvalidSurveyId() {
         // given
         setAuthority();
-        saveAccountYJ();
+        saveAccountYJ("duswo0624@naver.com");
 
         SurveyCategory surveyCategory = SurveyCategory.builder()
                 .type(SurveyCategoryType.NORMAL)
@@ -230,7 +248,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
     Collection<DynamicTest> createSubmitOrderIn() {
         // given
         setAuthority();
-        saveAccountYJ();
+        saveAccountYJ("duswo0624@naver.com");
 
 
         SurveyCategory surveyCategory = SurveyCategory.builder()
@@ -266,7 +284,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
     void notWinProbabilityZero() {
         // given
         setAuthority();
-        saveAccountYJ();
+        saveAccountYJ("duswo0624@naver.com");
         // 카테고리 생성
         SurveyCategory surveyCategory = SurveyCategory.builder()
                 .type(SurveyCategoryType.NORMAL)
@@ -317,7 +335,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
     void winProbability100Percent() {
         // given
         setAuthority();
-        saveAccountYJ();
+        saveAccountYJ("duswo0624@naver.com");
 
         SurveyCategory surveyCategory = SurveyCategory.builder()
                 .type(SurveyCategoryType.NORMAL)
@@ -376,7 +394,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
             void emptySurveyIsThrowError() {
                 // given
                 setAuthority();
-                saveAccountYJ();
+                saveAccountYJ("duswo0624@naver.com");
                 assertThatThrownBy(() -> surveyResultService.getSurveyResult(1L))
                         .isInstanceOf(BaseException.class)
                         .hasMessage("없는 설문입니다.");
@@ -388,7 +406,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
             void canCheckInstance() {
                 // given
                 setAuthority();
-                saveAccountYJ();
+                saveAccountYJ("duswo0624@naver.com");
                 SurveyCategory surveyCategory = SurveyCategory.builder()
                         .type(SurveyCategoryType.NORMAL)
                         .build();
@@ -405,7 +423,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
             void canCheckAnsweredSurvey() {
                 // given
                 setAuthority();
-                saveAccountYJ();
+                saveAccountYJ("duswo0624@naver.com");
                 SurveyCategory surveyCategory = SurveyCategory.builder()
                         .type(SurveyCategoryType.INSTANT_WIN)
                         .build();
@@ -426,7 +444,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
             void canCheckAnsweredSurvey() {
                 // given
                 setAuthority();
-                saveAccountYJ();
+                saveAccountYJ("duswo0624@naver.com");
 
                 SurveyCategory surveyCategory = SurveyCategory.builder()
                         .type(SurveyCategoryType.INSTANT_WIN)
@@ -470,7 +488,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
             void isWinTrue() {
                 // given
                 setAuthority();
-                saveAccountYJ();
+                saveAccountYJ("duswo0624@naver.com");
 
                 SurveyCategory surveyCategory = SurveyCategory.builder()
                         .type(SurveyCategoryType.INSTANT_WIN)
@@ -525,7 +543,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
             void emptySurveyIsThrowError() {
                 // given
                 setAuthority();
-                saveAccountYJ();
+                saveAccountYJ("duswo0624@naver.com");
                 assertThatThrownBy(() -> surveyResultService.patchSurveyResult(1L))
                         .isInstanceOf(BaseException.class)
                         .hasMessage("없는 설문입니다.");
@@ -537,7 +555,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
             void canCheckAnsweredSurvey() {
                 // given
                 setAuthority();
-                saveAccountYJ();
+                saveAccountYJ("duswo0624@naver.com");
                 SurveyCategory surveyCategory = SurveyCategory.builder()
                         .type(SurveyCategoryType.NORMAL)
                         .build();
@@ -556,7 +574,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
             void canCheckCloseSurvey() {
                 // given
                 setAuthority();
-                saveAccountYJ();
+                saveAccountYJ("duswo0624@naver.com");
                 SurveyCategory surveyCategory = SurveyCategory.builder()
                         .type(SurveyCategoryType.NORMAL)
                         .build();
@@ -574,7 +592,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
             void canCheckTypeIsNormal() {
                 // given
                 setAuthority();
-                saveAccountYJ();
+                saveAccountYJ("duswo0624@naver.com");
                 SurveyCategory surveyCategory = SurveyCategory.builder()
                         .type(SurveyCategoryType.INSTANT_WIN)
                         .build();
@@ -592,7 +610,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
             void canNotCheckResultNoAnsweredSurvey() {
                 // given
                 setAuthority();
-                saveAccountYJ();
+                saveAccountYJ("duswo0624@naver.com");
                 SurveyCategory surveyCategory = SurveyCategory.builder()
                         .type(SurveyCategoryType.NORMAL)
                         .build();
@@ -614,7 +632,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
             void isWinTrue() {
                 // given
                 setAuthority();
-                saveAccountYJ();
+                saveAccountYJ("duswo0624@naver.com");
 
                 SurveyCategory surveyCategory = SurveyCategory.builder()
                         .type(SurveyCategoryType.NORMAL)
@@ -654,6 +672,89 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
         }
     }
 
+    @DisplayName("설문ID를 이용하여 당첨자 목록을 조회한다.")
+    @WithMockUser(username = "duswo0624@naver.com")
+    @Test
+    void getWinningUsers() {
+        // given
+        setAuthority();
+        setAccountCode();
+        setSurveyCategory();
+        String email = "duswo0624@naver.com";
+        saveAccountYJ(email);
+
+        QuestionCreateServiceRequest questionCreateServiceRequestByShortForm = QuestionCreateServiceRequest.builder()
+                .questionNumber(1L)
+                .content("좋아하는 과일을 작성해주세요.")
+                .imgAddress(null)
+                .type(QuestionCategoryType.SHORT_FORM)
+                .build();
+        List<QuestionCreateServiceRequest> questionCreateServiceRequests = List.of(questionCreateServiceRequestByShortForm);
+
+        GiveawayType giveawayType = GiveawayType.COFFEE;
+        String giveawayName = "스타벅스 아메리카노";
+        int price = 4500;
+        GiveawayRequest request = GiveawayRequest.builder()
+                .giveawayType(giveawayType)
+                .name(giveawayName)
+                .price(price)
+                .build();
+        giveawayService.createGiveaway(request);
+        Optional<Giveaway> giveaway = giveawayRepository.findGiveawayByName(giveawayName);
+        Long giveawayId = giveaway.get().getId();
+        GiveawayAssignServiceRequest giveawayAssignServiceRequest = GiveawayAssignServiceRequest.builder()
+                .id(giveawayId)
+                .count(1)
+                .build();
+        List<GiveawayAssignServiceRequest> giveawayAssignServiceRequests = List.of(giveawayAssignServiceRequest);
+
+        LocalDateTime now = LocalDateTime.now();
+        String surveyTitle = "당신은 어떤 과일을 좋아하나요?";
+        SurveyCreateServiceRequest surveyCreateServiceRequest = SurveyCreateServiceRequest.builder()
+                .title(surveyTitle)
+                .startTime(now.minusDays(1))
+                .endTime(now.plusDays(1))
+                .headCount(0)
+                .surveyTarget(List.of(AccountCodeType.MAN, AccountCodeType.WOMAN, AccountCodeType.TWENTIES))
+                .closedHeadCount(1)
+                .type(SurveyCategoryType.NORMAL)
+                .questions(questionCreateServiceRequests)
+                .giveaways(giveawayAssignServiceRequests)
+                .build();
+
+        SurveyResponse saveSurvey = surveyService.addSurvey(surveyCreateServiceRequest);
+        Long surveyId = saveSurvey.getId();
+        surveyRepository.findById(surveyId).get().toOpen();
+
+        QuestionAnswerCreateServiceRequest questionAnswerCreateServiceRequest
+                = QuestionAnswerCreateServiceRequest
+                .builder()
+                .questionId(saveSurvey.getQuestions().get(0).getId())
+                .multipleChoiceAnswer(null)
+                .shorFormAnswer("치즈케익")
+                .build();
+        questionAnswerService.addQuestionAnswer(LocalDateTime.now(), surveyId, List.of(questionAnswerCreateServiceRequest));
+
+        // when
+        List<WinningAccountResponse> winningUsers = surveyResultService.getWinningUsers(surveyId);
+
+        // then
+        assertThat(winningUsers.size()).isOne();
+        assertThat(winningUsers.get(0).getEmail()).isEqualTo(email);
+    }
+
+    @DisplayName("존재하지 않는 설문의 경우 당첨자 목록을 조회할 수 없다.")
+    @Test
+    void cannotGetWinningUsersWithInvalidSurveyId() {
+        // given
+        Long invalidSurveyId = 1L;
+
+        // when // then
+        assertThatThrownBy(() -> surveyResultService.getWinningUsers(invalidSurveyId))
+                .isInstanceOf(BaseException.class)
+                .hasMessage("없는 설문입니다.");
+    }
+
     private Survey getSurvey(int headCount, int closedHeadCount, SurveyCategory surveyCategory) {
         Survey survey = Survey.builder()
                 .title("당신은 어떤 과일을 좋아하십니까?")
@@ -668,8 +769,7 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
         return survey;
     }
 
-    private void saveAccountYJ() {
-        String email = "duswo0624@naver.com";
+    private void saveAccountYJ(String email) {
         AccountCreateServiceRequest accountCreateServiceRequest = AccountCreateServiceRequest.builder()
                 .email(email)
                 .password("1234")
@@ -701,5 +801,25 @@ class SurveyResultServiceTest extends IntegrationTestSupport {
                 .build();
 
         authorityRepository.save(userAuthority);
+    }
+
+    private void setAccountCode() {
+        List<AccountCodeType> accountCodeTypes = List.of(AccountCodeType.MAN, AccountCodeType.WOMAN, AccountCodeType.UNDER_TEENS,
+                AccountCodeType.TEENS, AccountCodeType.TWENTIES, AccountCodeType.THIRTIES,
+                AccountCodeType.FORTIES, AccountCodeType.FIFTIES, AccountCodeType.SIXTIES);
+        for (AccountCodeType accountCodeType : accountCodeTypes) {
+            accountCodeRepository.save(AccountCode.builder()
+                    .type(accountCodeType)
+                    .build());
+        }
+    }
+
+    private void setSurveyCategory() {
+        surveyCategoryRepository.save(SurveyCategory.builder()
+                .type(SurveyCategoryType.NORMAL)
+                .build());
+        surveyCategoryRepository.save(SurveyCategory.builder()
+                .type(SurveyCategoryType.INSTANT_WIN)
+                .build());
     }
 }
