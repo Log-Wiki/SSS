e2bf6b190d5ffeb29acff44580408fed7d9748c7
KWON SEONGEUN <kss4037@gmail.com>
2023-08-14 10:22:17 +0900

    :ambulance: 결제 검증 요청에 설문ID 추가

--- src/main/java/com/logwiki/specialsurveyservice/api/controller/payment/request/PaymentAuthenticationRequest.java
+++ src/main/java/com/logwiki/specialsurveyservice/api/controller/payment/request/PaymentAuthenticationRequest.java
@@ -2,6 +2,7 @@ package com.logwiki.specialsurveyservice.api.controller.payment.request;
 
 import com.logwiki.specialsurveyservice.api.service.payment.request.PaymentAuthenticationServiceRequest;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class PaymentAuthenticationRequest {
+    @NotNull(message = "설문 ID는 필수입니다.")
+    private Long surveyId;
 
     @NotEmpty(message = "주문 ID는 필수입니다.")
     private String orderId;
@@ -18,13 +21,15 @@ public class PaymentAuthenticationRequest {
 
 
     @Builder
-    public PaymentAuthenticationRequest(String orderId, String impUid) {
+    public PaymentAuthenticationRequest(Long surveyId, String orderId, String impUid) {
+        this.surveyId = surveyId;
         this.orderId = orderId;
         this.impUid = impUid;
     }
 
     public PaymentAuthenticationServiceRequest toServiceRequest() {
         return PaymentAuthenticationServiceRequest.builder()
+                .surveyId(surveyId)
                 .orderId(orderId)
                 .impUid(impUid)
                 .build();
