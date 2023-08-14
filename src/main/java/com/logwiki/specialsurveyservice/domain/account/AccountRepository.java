10ba7dcd59d0dc2d9140c5887e7f3ea3b9016046
KWON SEONGEUN <kss4037@gmail.com>
2023-08-14 12:22:52 +0900

    :sparkles: 설문 ID에 대해 응답이 가능한 account들 조회 추가

--- src/main/java/com/logwiki/specialsurveyservice/domain/account/AccountRepository.java
+++ src/main/java/com/logwiki/specialsurveyservice/domain/account/AccountRepository.java
@@ -1,8 +1,12 @@
 package com.logwiki.specialsurveyservice.domain.account;
 
+import com.logwiki.specialsurveyservice.domain.survey.Survey;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,4 +17,16 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 
     @EntityGraph(attributePaths = "authorities")
     Optional<Account> findOneWithAuthoritiesByPhoneNumber(String phoneNumber);
+
+    @Query(value = "SELECT * FROM account acc "
+            + "WHERE (SELECT ac.ID FROM ACCOUNT_CODE ac WHERE ac.TYPE = acc.gender) IN (SELECT st.ACCOUNT_CODE_ID "
+            + "FROM SURVEY_TARGET st "
+            + "WHERE st.SURVEY_ID = :surveyId)"
+            + "AND (SELECT ac.ID FROM ACCOUNT_CODE ac WHERE ac.TYPE = acc.age) IN (SELECT st.ACCOUNT_CODE_ID "
+            + "FROM SURVEY_TARGET st "
+            + "WHERE st.SURVEY_ID = :surveyId)"
+           ,
+            nativeQuery = true)
+    List<Account> findRecommendAccounts(@Param("surveyId") Long surveyId);
+
 }
